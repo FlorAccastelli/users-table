f15b8ee0385bac0e17da1168d75d0e0dfe97d2b3
Ianduha13 <ianduhamelhayes@gmail.com>
2024-08-08 20:44:11 -0300

    refactor: query params and serverside improvements

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,33 @@
 import Table from "@/components/Table";
 
-export default async function Home() {
+const fetchData = async () => {
+  "use server"
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await res.json();
+  const mappedData = data.reduce((acc, x) => {
+    return [...acc, {
+      id: x.id,
+      name: x.name,
+      username: x.username,
+      email: x.email,
+      address: x.address.city,
+      phone: x.phone,
+      website: x.website,
+      company: x.company.name
+    }]
+  }, [])
 
-  const fetchData = async () => {
-    const res = await fetch ("https://jsonplaceholder.typicode.com/users")
-    const data = await res.json();
-    return data;
-  }
+  return mappedData
+}
 
+export default async function Home({ searchParams }) {
   const data = await fetchData()
+  const dataFiltered = data.filter((d) => (
+    d.username.toLowerCase().match(searchParams.search?.toLowerCase() || "")
+  ))
+  const titles = Object.keys(data[0]);
 
   return (
-    <Table data={data}/>
+    <Table data={dataFiltered} titles={titles} />
   );
 }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,50 +1,12 @@
 export default function Users({ user, activeCols }) {
-    const styles = "font-light text-xs p-2 border-b border-gray-200";
-    console.log("active cols desde users", activeCols)
+    const styles = "font-light text-xs p-2 border-b border-gray-200 text-center";
     return (
-    <tr key={user.id} className="bg-white hover:bg-gray-100">
-       
-        {activeCols.id &&
+        <tr key={user.id} className="bg-white hover:bg-gray-100">
+            {activeCols.map((col) =>
             (<td key={user.id} className={styles + " h-12"}>
-                {user.id}
-            </td>)
-        }
-        {activeCols.name && 
-            (<td key={user.name} className={styles + " h-12"}>
-                {user.name}
-            </td>)
-        }
-        {activeCols.username && 
-            (<td key={user.username} className={styles + " h-12"}>
-                {user.username}
-            </td>)
-        }
-        {activeCols.email && 
-            (<td key={user.email} className={styles + " h-12"}>
-                {user.email}
-            </td>)
-        }
-        {activeCols.address && 
-            (<td key={user.address.city} className={styles + " h-12"}>
-                {user.address.city}
-            </td>)
-        }
-        {activeCols.phone && 
-            (<td key={user.id} className={styles + " h-12"}>
-                {user.phone}
-            </td>)
-        }
-        {activeCols.website && 
-            (<td key={user.website} className={styles + " h-12"}>
-                {user.website}
-            </td>)
-        }
-        {activeCols.company && 
-            (<td key={user.company.name} className={styles + " h-12"}>
-                {user.company.name}
-            </td>)
-        }
-    </tr>
-    
-  )
+                {user[col]}
+            </td>))
+            }
+        </tr>
+    )
 }
