e3c424e8504e8ad938900135bfd896c8131c41db
Florencia Accastelli <f.accastelli@gmail.com>
2024-08-08 11:07:18 -0300

    rendering structure

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,22 +1,59 @@
 export default function Users({ user }) {
     const styles = "font-light text-xs p-2 border-b border-gray-200";
-    const userData = [
-        user.id,
-        user.name,
-        user.username,
-        user.email,
-        user.address.street,
-        user.phone,
-        user.website,
-        user.company.name
-    ]
+
+    const activeCols = {
+        id: true,
+        name: true,
+        username: true,
+        email: true,
+        address: true,
+        phone: true,
+        website: true,
+        company: true
+    }
   return (
     <tr key={user.id} className="bg-white hover:bg-gray-100">
-        {userData.map((data, index)=> (
-            <th key={index} className={styles + " h-12"}>
-                {data}
-            </th>
-        ))}
+       
+        {activeCols.id && 
+            (<td key={user.id} className={styles + " h-12"}>
+                {user.id}
+            </td>)
+        }
+        {activeCols.name && 
+            (<td key={user.id} className={styles + " h-12"}>
+                {user.name}
+            </td>)
+        }
+        {activeCols.username && 
+            (<td key={user.id} className={styles + " h-12"}>
+                {user.username}
+            </td>)
+        }
+        {activeCols.email && 
+            (<td key={user.id} className={styles + " h-12"}>
+                {user.email}
+            </td>)
+        }
+        {activeCols.address && 
+            (<td key={user.id} className={styles + " h-12"}>
+                {user.address.city}
+            </td>)
+        }
+        {activeCols.phone && 
+            (<td key={user.id} className={styles + " h-12"}>
+                {user.phone}
+            </td>)
+        }
+        {activeCols.website && 
+            (<td key={user.id} className={styles + " h-12"}>
+                {user.website}
+            </td>)
+        }
+        {activeCols.company && 
+            (<td key={user.id} className={styles + " h-12"}>
+                {user.company.name}
+            </td>)
+        }
     </tr>
   )
 }
